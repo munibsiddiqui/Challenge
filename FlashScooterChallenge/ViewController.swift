95ff8c63552193d4688c1e2b69f9c730bf8d6f33
Munib Siddiqui <munibsiddiqui.gt@gmail.com>
2019-07-09 09:47:37 +0500

    Initial Commit

---
+++ FlashScooterChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashScooterChallenge
+//
+//  Created by Gaditek on 09/07/2019.
+//  Copyright © 2019 Munib Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
